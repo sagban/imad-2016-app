d4e67e23741e2f4d83659b1ce9c2e89cdbb040a1
sagban <sagarbansal099@gmail.com>
2016-09-22 17:04:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'));
 });
 
 
