c5f7589aaaccdf0f633bfab7496cac63628b3abc
sagban <sagarbansal099@gmail.com>
2016-09-29 23:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-<!doctype html>
-<html>
-    <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="center">
-            <img src="/googlelogo_color_272x92dp.png" class="img-medium"/>
-        </div>
-        <br>
-        <div class="center text-big bold">
-            Hi! I am sagar bansal.THIS IS MY
-        </div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
-</html>
+
