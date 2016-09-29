53e2836dea9d5275774b52fe9348d642570d0acf
sagban <sagarbansal099@gmail.com>
2016-09-29 23:26:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+var express = required(`express`);
+var morgan = required(`morgan`);
+var path = required(`path`);
 
+var app = express();
+app.use(morgan(`combined`));
+
+app.get(`/`, function (req, res){
