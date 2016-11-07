1e8ea9bda7766469f8fc603533faa7b9cf41e433
sagban <sagarbansal099@gmail.com>
2016-11-07 12:50:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ var app = express();
 app.use(morgan(`combined`));
 
 app.get(`/`, function (req, res){
+    res.sendFile(path.join(__dirname,'ui','index.html'));
+});
+app.get(`/ui/styl.css`, function (req, res){
+    res.sendFile(path.join(__dirname,'ui','style.css'));
+});
+app.get(`/ui/madi.png`, function (req, res){
+    res.sendFile(path.join(__dirname,'ui','madi.png'));
+});
+
+var port = 800;
+app.listen(8080, function (){
+    console.log('IMAD course app listening on port ${port}!');
+});
