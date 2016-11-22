fbb6e6249f14ee272a78a1e0c442e6f648bdad7a
sagban <sagarbansal099@gmail.com>
2016-11-22 17:15:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.get('/sagban', function (req, res) {
 app.get('/uk', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'uk.html'));
 });
-
+app.get('/varsha', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'VARSHA.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
